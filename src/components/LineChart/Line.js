c262fa18f520533666a3efcd3285ba7e8f0477ca
fazzyA <faz.pak@gmail.com>
2021-09-28 13:18:09 +0500

    line simple chart

--- src/components/LineChart/Line.js
+++ src/components/LineChart/Line.js
@@ -13,17 +13,22 @@ function Line() {
     for (let i = 0; i < 20; i++) {
       const value = Math.floor(Math.random() * i + 3);
       chartData.push({
+        date: new Date(),
         label: i,
         value,
         tooltipContent: `<b>x: </b>${i}<br><b>y: </b>${value}`
       });
     }
     setData(chartData)
+    // const line = d3.line()
+    // .x(d => x(d.date))
+    // .y(d => y(d.value));  
   }
-
+ //sample data [[10, 60], [20, 70], [30, 80], [40, 100], [40, 100], [50, 120], [60, 200]]
   return (
     <div className="App">
-      <button onClick={regenerateData}>Change Data</button>
+      {/* <button onClick={regenerateData}>Change Data</button> */}
+
       <LineChart data={data} width={400} height={300} />
     </div>
   );
--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -1,18 +1,30 @@
 import React, { useEffect } from 'react';
+import * as d3 from "d3";
 
 // import './index.css';
 
 function LineChart(props) {
-  const { data } = props;
+  const { width, height } = props;
+  // const data = [
+  //   {date: new Date(2007, 3, 24), value: 93.24},
+  //   {date: new Date(2007, 3, 25), value: 95.35},
+  //   {date: new Date(2007, 3, 26), value: 98.84},
+  //   {date: new Date(2007, 3, 27), value: 99.92},
+  //   {date: new Date(2007, 3, 30), value: 99.80},
+  //   {date: new Date(2007, 4,  1), value: 99.47},
+  // ];
+  // const line = d3.line()
+  //   .x(d => x(d.date))
+  //   .y(d => y(d.value));
 
-  useEffect(() => {
-    drawChart();
-  }, [data]);
-  
-  function drawChart() {
-    // Add logic to draw the chart here
-  }
-  return <div id="container" />;
+const data = [[10, 60], [20, 70], [30, 80], [40, 100], [40, 100], [50, 120], [60, 200]];
+  const line = d3.line()(data)
+
+  return (
+  <svg width={width} height={height}>
+    <path d={line} fill={d3.schemeCategory10} />
+  </svg>
+  );
 }
 
 export default LineChart;
